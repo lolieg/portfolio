cfe735f2cbc821fbd83638fcb869f6c804277462
lolieg <marvinlohmar123456789@gmail.com>
2021-01-14 22:16:51 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ post.title }}</h1>
+    <p>{{ post.description }}</p>
+    <NuxtLink to="/lmao"></NuxtLink>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  async asyncData({ app }) {
+    const post = await app.$http.$get(`https://api.nuxtjs.dev/posts/${1}`)
+    return { post }
+  },
+})
+// ...
+</script>
+
+<style>
+.red {
+  color: red;
+}
+</style>
