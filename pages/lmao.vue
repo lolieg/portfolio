1cb2222e5a86e53cd3eebd2f7aa05e3369c5e2cf
lolieg <marvinlohmar123456789@gmail.com>
2021-01-14 22:18:07 +0100

    iniT

---
+++ pages/lmao.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NuxtLink to="/">Back to Home</NuxtLink>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style scoped>
+.links {
+  padding-top: 15px;
+}
+</style>
